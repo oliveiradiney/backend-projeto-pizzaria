416cbabcb22059e45e46e812d91c84d4562fae21
oliveiradiney <dineyoliveira1999@gmail.com>
2022-08-10 23:29:26 -0300

    Criptografando a senha e criando o login do usuario

---
+++ src/services/users/AuthUserService.ts
@@ -0,0 +1,36 @@
+import prismaClient from "../../prisma";
+import { compare } from 'bcryptjs'
+
+
+interface AuthRequest{
+    email: string;
+    password: string;
+}
+
+
+class AuthUserService{
+    async execute({email, password}: AuthRequest){
+        //verificar se o email existe.
+        const user = await prismaClient.user.findFirst({
+            where:{
+                email: email
+            }
+        })
+
+        if(!user){
+            throw new Error("Usuario/senha incorreto")
+        }
+
+        //preciso verificar se a senha que ele mandou está correta
+        const passwordMatch = await compare(password, user.password)
+        if(!passwordMatch){
+            throw new Error("Usuario/senha incorreto")
+        }
+        //gerar um token JWT e devolver os dados do usuario como id, name e email
+
+
+        return { ok: true }
+    }
+}
+
+export {AuthUserService}
--- src/services/users/createUserService.ts
+++ src/services/users/createUserService.ts
@@ -1,4 +1,5 @@
 import  prismaClient from '../../prisma';
+import { hash } from 'bcryptjs';
 
 interface UserRequest{
     name: string;
@@ -26,11 +27,13 @@ class CreateUserService{
             throw new Error("Usuário ja existe")
         }
 
+        const passwordHash = await hash(password, 8)
+
         const user = await prismaClient.user.create({
             data:{
                 name:name,
                 email: email,
-                password: password
+                password: passwordHash,
             },
             select:{
                 id: true,
