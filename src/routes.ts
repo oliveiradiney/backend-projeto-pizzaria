a5df16ed5a7acc06f51e4a91a0d8a9f9042498a8
oliveiradiney <dineyoliveira1999@gmail.com>
2022-08-20 18:44:05 -0300

    Listando pedidos enviados para mesa

--- src/routes.ts
+++ src/routes.ts
@@ -15,7 +15,9 @@ import { CreateOrderController } from './controllers/order/CreateOrderController
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
 import { AddItemController } from './controllers/order/AddItemController';
 import { RemoveItemController } from './controllers/order/RemoveItemController';
+
 import { SendOrderController } from './controllers/order/SendOrderController';
+import { ListOrderController } from './controllers/order/ListOrderController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
@@ -51,7 +53,10 @@ router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
 router.post('/order/add', isAuthenticated, new AddItemController().handle)
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
+
 router.put('/order/send', isAuthenticated, new SendOrderController().handle)
+router.get('/orders', isAuthenticated, new ListOrderController().handle)
+
 
 export {
     router
